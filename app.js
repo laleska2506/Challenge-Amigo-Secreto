dff37660cb6b115711341722974a59229a335e41
Laleska <laleskaarroyoaquino0@gmail.com>
2025-03-02 15:37:07 -0500

    Crear funcion mostrarAmigo

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ function agregarAmigo(){
     //documento.getElementById.value = ayuda a traer el valor del id 
     var amigoIngresado = document.getElementById("amigo").value;
 
-    if(amigoIngresado == ''){
+    if(amigoIngresado === ''){
         alert("Debes ingresar un nombre");
     }else{
-    amigos.push(amigoIngresado);
-    console.log(amigos);
+        if (amigos.includes(amigoIngresado)){
+            alert("Ese amigo ya fue ingresado. Ingrese uno nuevo");
+        } else{
+            amigos.push(amigoIngresado);
+            console.log(amigos);
+            mostrarAmigo();
+        }
+    }
+}
+
+function mostrarAmigo(){
+    var mostrarAmigo = document.getElementById("listaAmigos"); //obteine la lista
+    mostrarAmigo.innerHTML = ""     //limpia la lista antes de agregar nuevos
+
+    for (let i = 0; i< amigos.length;i++){
+        let li = document.createElement("li");   //crea elemtno li
+        li.textContent = amigos[i];     //soloo agrego un nombre a la vez
+        mostrarAmigo.appendChild(li);
     }
 }
